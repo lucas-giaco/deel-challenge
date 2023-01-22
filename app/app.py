51d67895accc74a9b54607b04ec2f5b13d33a8f4
Lucas Giacomazzi <lucasgiacomazzi@gmail.com>
2023-01-22 11:09:00 -0300

    Add persistence to app

--- app/app.py
+++ app/app.py
@@ -1,5 +1,5 @@
 import os
-# from mysql.connector import connect, Error
+from mysql.connector import connect, Error
 from flask import Flask, request
 
 app = Flask(__name__)
@@ -22,22 +22,34 @@ def reverse_ip():
         ip = request.remote_addr
     ip_list = ip.split(".")
     ip_list.reverse()
-    return ".".join(ip_list)
-    # try:
-    #     with connect(
-    #       host=os.getenv('DB_HOST', 'localhost'),
-    #       port=int(os.getenv('DB_PORT', '3306')),
-    #       user=os.getenv('DB_USER'),
-    #       password=os.getenv('DB_PASSWORD')
-    #     ) as connection:
-    #         if connection.is_connected():
-    #             db_Info = connection.get_server_info()
-    #             return f"Connected to MySQL Server version {db_Info}"
-    # except Error as e:
-    #     print(e)
-    #     return 'Unable to connect to DB'
+    reversed_ip = ".".join(ip_list)
+
+    try:
+        store_ip(ip, reversed_ip)
+    except Error as e:
+        return e.msg, 500
+
+    return f'{ip} => {reversed_ip}'
+
+def store_ip(ip, reversed_ip):
+    """Store an IP and its reversed IP into a DB"""
+    if bool(os.getenv('STORAGE_ENABLED', 'False')):
+        with connect(
+            host=os.getenv('STORAGE_HOST', 'localhost'),
+            port=int(os.getenv('STORAGE_PORT', '3306')),
+            database=os.getenv('STORAGE_DATABASE'),
+            user=os.getenv('STORAGE_USER'),
+            password=os.getenv('STORAGE_PASSWORD')
+        ) as connection:
+            with connection.cursor() as cursor:
+                cursor.execute(
+                    "INSERT INTO ips(ip, reverse_ip) VALUES(%s, %s)",
+                    (ip, reversed_ip)
+                )
+                connection.commit()
 
 if __name__ == '__main__':
     host = os.getenv('HOST_IP', '0.0.0.0')
     port = int(os.getenv('PORT', '5000'))
-    app.run(host=host, port=port, debug=True)
+    debug = bool(os.getenv('DEBUG', 'False'))
+    app.run(host=host, port=port, debug=debug)
